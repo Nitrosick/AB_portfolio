d828a83da0716ba6e10899c4eed014b4e54c352f
Nikita Buzhinskiy <n.buzhinskiy@ruvents.com>
2025-05-18 17:41:49 +0400

    New project added

--- const/projects.js
+++ const/projects.js
@@ -1,5 +1,31 @@
 export default [
-  { id: 1, name: 'Personal Portfolio', category: 'landing page', link: 'https://nb-portfolio.ru/', description: 'A responsive landing page design for a web developer`s portfolio, showcasing their skills and projects.' },
-  { id: 2, name: 'Memphis', category: 'landing page - case study', link: 'https://www.behance.net/gallery/216362803/Memphis-Landing-A-Journey-into-Bold-and-Playful-UI', description: 'A landing page dedicated to Memphis Style in web design with bold visuals and a user-friendly webinar registration flow.' },
-  { id: 3, name: 'SubScout', category: 'landing page - case study', link: 'https://www.figma.com/proto/EZRkCQ85spoyWGzOhUkXDe/Subscout-App-%D0%A4%D0%B8%D0%BD%D0%B0%D0%BB%D1%8C%D0%BD%D1%8B%D0%B9-%D0%BF%D1%80%D0%BE%D0%B5%D0%BA%D1%82-7-%D1%81%D0%BF%D1%80%D0%B8%D0%BD%D1%82%D0%B0.-%D0%90%D0%BB%D0%B5%D0%BA%D1%81%D0%B0%D0%BD%D0%B4%D1%80%D0%B0-%D0%91%D1%83%D0%B6%D0%B8%D0%BD%D1%81%D0%BA%D0%B0%D1%8F-59-%D0%BA%D0%BE%D0%B3%D0%BE%D1%80%D1%82%D0%B0?node-id=9395-7742&p=f&t=Wsf4dTvJNVvEmuVX-0&scaling=min-zoom&content-scaling=fixed&starting-point-node-id=9385%3A5636', description: 'A subscription tracking app designed with iOS guidelines for seamless management and control.', single: true }
+  {
+    id: 1,
+    name: 'Watch Together',
+    category: 'feature design - case study',
+    link: 'https://www.behance.net/gallery/226037827/UXUI-Case-Study-Feature-for-online-movie-co-watching',
+    description: 'Feature design for shared video watching inside a live product ecosystem. Created within an existing design system, the work included user flow planning, admin-user interaction, and adaptive UI.',
+    single: true
+  },
+  {
+    id: 2,
+    name: 'Personal Portfolio',
+    category: 'landing page',
+    link: 'https://nb-portfolio.ru/',
+    description: 'A responsive landing page design for a web developer`s portfolio, showcasing their skills and projects.'
+  },
+  {
+    id: 3,
+    name: 'Memphis',
+    category: 'landing page - case study',
+    link: 'https://www.behance.net/gallery/216362803/Memphis-Landing-A-Journey-into-Bold-and-Playful-UI',
+    description: 'A landing page dedicated to Memphis Style in web design with bold visuals and a user-friendly webinar registration flow.'
+  },
+  {
+    id: 4,
+    name: 'SubScout',
+    category: 'mobile app - case study',
+    link: 'https://www.figma.com/proto/EZRkCQ85spoyWGzOhUkXDe/Subscout-App-%D0%A4%D0%B8%D0%BD%D0%B0%D0%BB%D1%8C%D0%BD%D1%8B%D0%B9-%D0%BF%D1%80%D0%BE%D0%B5%D0%BA%D1%82-7-%D1%81%D0%BF%D1%80%D0%B8%D0%BD%D1%82%D0%B0.-%D0%90%D0%BB%D0%B5%D0%BA%D1%81%D0%B0%D0%BD%D0%B4%D1%80%D0%B0-%D0%91%D1%83%D0%B6%D0%B8%D0%BD%D1%81%D0%BA%D0%B0%D1%8F-59-%D0%BA%D0%BE%D0%B3%D0%BE%D1%80%D1%82%D0%B0?node-id=9395-7742&p=f&t=Wsf4dTvJNVvEmuVX-0&scaling=min-zoom&content-scaling=fixed&starting-point-node-id=9385%3A5636', description: 'A subscription tracking app designed with iOS guidelines for seamless management and control.',
+    single: true
+  }
 ]
