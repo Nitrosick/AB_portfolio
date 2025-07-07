60e8c1ae23755a76600066c87f2c66af9c2d2323
Nikita Buzhinskiy <n.buzhinskiy@ruvents.com>
2025-07-07 11:10:50 +0400

    CV changed

--- const/projects.js
+++ const/projects.js
@@ -58,17 +58,17 @@ export default [
       ]
     }
   },
-  {
-    id: 4,
-    name: 'SubScout',
-    category: 'mobile app - case study',
-    link: 'https://www.figma.com/proto/EZRkCQ85spoyWGzOhUkXDe/Subscout-App-%D0%A4%D0%B8%D0%BD%D0%B0%D0%BB%D1%8C%D0%BD%D1%8B%D0%B9-%D0%BF%D1%80%D0%BE%D0%B5%D0%BA%D1%82-7-%D1%81%D0%BF%D1%80%D0%B8%D0%BD%D1%82%D0%B0.-%D0%90%D0%BB%D0%B5%D0%BA%D1%81%D0%B0%D0%BD%D0%B4%D1%80%D0%B0-%D0%91%D1%83%D0%B6%D0%B8%D0%BD%D1%81%D0%BA%D0%B0%D1%8F-59-%D0%BA%D0%BE%D0%B3%D0%BE%D1%80%D1%82%D0%B0?node-id=9395-7742&p=f&t=Wsf4dTvJNVvEmuVX-0&scaling=min-zoom&content-scaling=fixed&starting-point-node-id=9385%3A5636', description: 'A subscription tracking app designed with iOS guidelines for seamless management and control.',
-    single: true,
-    details: {
-      title: '',
-      blocks: [
-        { title: '', text: '' }
-      ]
-    }
-  }
+  // {
+  //   id: 4,
+  //   name: 'SubScout',
+  //   category: 'mobile app - case study',
+  //   link: 'https://www.figma.com/proto/EZRkCQ85spoyWGzOhUkXDe/Subscout-App-%D0%A4%D0%B8%D0%BD%D0%B0%D0%BB%D1%8C%D0%BD%D1%8B%D0%B9-%D0%BF%D1%80%D0%BE%D0%B5%D0%BA%D1%82-7-%D1%81%D0%BF%D1%80%D0%B8%D0%BD%D1%82%D0%B0.-%D0%90%D0%BB%D0%B5%D0%BA%D1%81%D0%B0%D0%BD%D0%B4%D1%80%D0%B0-%D0%91%D1%83%D0%B6%D0%B8%D0%BD%D1%81%D0%BA%D0%B0%D1%8F-59-%D0%BA%D0%BE%D0%B3%D0%BE%D1%80%D1%82%D0%B0?node-id=9395-7742&p=f&t=Wsf4dTvJNVvEmuVX-0&scaling=min-zoom&content-scaling=fixed&starting-point-node-id=9385%3A5636', description: 'A subscription tracking app designed with iOS guidelines for seamless management and control.',
+  //   single: true,
+  //   details: {
+  //     title: '',
+  //     blocks: [
+  //       { title: '', text: '' }
+  //     ]
+  //   }
+  // }
 ]
